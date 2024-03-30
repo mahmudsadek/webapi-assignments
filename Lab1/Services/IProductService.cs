6da5546cb3cf14c80d1b4a5c7c0d5719a4c769a6
sadek <mahmoudsadek.1905@gmail.com>
2024-03-30 16:47:24 +0200

    add func to service layer

--- Lab1/Services/IProductService.cs
+++ Lab1/Services/IProductService.cs
@@ -1,9 +1,21 @@
 ﻿using Lab1.Models;
+using Lab1.ViewModels;
 
 namespace Lab1.Services
 {
     public interface IProductService
     {
         IEnumerable<Product> GetAll(); 
+        Product GetById(int id);
+
+        Product Insert(ProductViewModel newProd);
+        void Insert(Product newProd);
+
+
+        void Update(Product product ,ProductViewModel newProd);
+
+        void Delete(int id);
+
+        IEnumerable<Product> Get(Func<Product,bool> where);
     }
 }
--- Lab1/Services/ProductService.cs
+++ Lab1/Services/ProductService.cs
@@ -1,19 +1,57 @@
 ﻿using Lab1.Models;
 using Lab1.Repositories;
+using Lab1.ViewModels;
 
 namespace Lab1.Services
 {
     public class ProductService : IProductService
     {
         public readonly IProductRepository ProductRepository;
-        public ProductService(IProductRepository productRepository) 
+        public ProductService(IProductRepository productRepository)
         {
             ProductRepository = productRepository;
         }
 
         public IEnumerable<Product> GetAll()
         {
-            return ProductRepository.GetAll();
+            return ProductRepository.GetAll(p => p.isDeleted == false);
+        }
+
+        public Product GetById(int id)
+        {
+            return ProductRepository.GetById(id, p => p.Id == id && p.isDeleted == false);
+        }
+
+        public IEnumerable<Product> Get(Func<Product, bool> where)
+        {
+            return ProductRepository.Get(where);
+        }
+        public Product Insert(ProductViewModel newProd)
+        {
+            Product product = new Product()
+            { Name = newProd.Name, Description = newProd.Description, Price = newProd.Price };
+            ProductRepository.Insert(product);
+            ProductRepository.Save();
+            return product;
+        }
+        public void Insert(Product newProd)
+        {
+            ProductRepository.Insert(newProd);
+            ProductRepository.Save();
+        }
+
+            public void Update(Product product,ProductViewModel newProd)
+        {
+            product.Name = newProd.Name;
+            product.Description = newProd.Description;
+            product.Price = newProd.Price;
+            ProductRepository.Update(product);
+            ProductRepository.Save();
+        }
+
+        public void Delete(int id) 
+        {
+            ProductRepository.SoftDelete(id);
         }
     }
 }
