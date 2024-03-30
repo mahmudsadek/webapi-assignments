3659c6d2b929ec5b8ea8ec37a711a3880bbbfb55
sadek <mahmoudsadek.1905@gmail.com>
2024-03-30 16:47:45 +0200

    add viewmodel

---
+++ Lab1/ViewModels/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab1.ViewModels
+{
+    public class ProductViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        [Column(TypeName = "money")]
+        public decimal Price { get; set; }
+    }
+}
