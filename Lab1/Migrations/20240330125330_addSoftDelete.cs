1d24a56b0f03e45588106de4cfafbd18e536e327
sadek <mahmoudsadek.1905@gmail.com>
2024-03-30 16:46:15 +0200

    add migration

---
+++ Lab1/Migrations/20240330125330_addSoftDelete.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Lab1.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSoftDelete : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedTime",
+                table: "Product",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "isDeleted",
+                table: "Product",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedTime",
+                table: "Product");
+
+            migrationBuilder.DropColumn(
+                name: "isDeleted",
+                table: "Product");
+        }
+    }
+}
