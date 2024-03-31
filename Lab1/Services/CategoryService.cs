67e17228a97a7539a092e116c96b41681bce6036
sadek <mahmoudsadek.1905@gmail.com>
2024-03-31 15:17:50 +0200

    add category service

---
+++ Lab1/Services/CategoryService.cs
@@ -0,0 +1,44 @@
+﻿using Lab1.DTO;
+using Lab1.Models;
+using Lab1.Repositories;
+
+namespace Lab1.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private readonly ICategoryRepository categoryRepository;
+
+        public CategoryService(ICategoryRepository categoryRepository)
+        {
+            this.categoryRepository = categoryRepository;
+        }
+
+        public CategoryWithProductNameDTO GetById(int id)
+        {
+            Category category = categoryRepository.GetCateWithProducts(id);
+            if (category == null)
+            {
+                return null;
+            }
+            CategoryWithProductNameDTO cat = new CategoryWithProductNameDTO() 
+            {Id = category.Id,Name = category.Name , ProductNames = new List<string>()};
+            if (category.Products != null)
+            { 
+                foreach (Product p in category.Products)
+                {
+                    cat.ProductNames.Add(p.Name);
+                }
+            }
+            return cat;
+        }
+
+        public Category Insert(CategoryWithoutId category)
+        {
+            Category cat = new Category()
+            { Name = category.Name};
+            categoryRepository.Insert(cat);
+            categoryRepository.Save();
+            return cat;
+        }
+    }
+}
---
+++ Lab1/Services/ICategoryService.cs
@@ -0,0 +1,11 @@
+﻿using Lab1.DTO;
+using Lab1.Models;
+
+namespace Lab1.Services
+{
+    public interface ICategoryService
+    {
+        CategoryWithProductNameDTO GetById(int id);
+        Category Insert(CategoryWithoutId category);
+    }
+}
