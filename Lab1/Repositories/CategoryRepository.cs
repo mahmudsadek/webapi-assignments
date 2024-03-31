d8c9692d801a3cde6324dbab378a29307110798c
sadek <mahmoudsadek.1905@gmail.com>
2024-03-31 15:17:13 +0200

    add cate repo and icat repo

---
+++ Lab1/Repositories/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using Lab1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab1.Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(Context _context) : base(_context)
+        {
+        }
+
+        public Category GetCateWithProducts(int id)
+        {
+            return Context.Category.Include(c=>c.Products).FirstOrDefault(c=>c.Id == id);
+        }
+    }
+}
---
+++ Lab1/Repositories/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Lab1.Models;
+
+namespace Lab1.Repositories
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        Category GetCateWithProducts(int id);
+    }
+}
