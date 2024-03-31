58030787b1cf9d52eb1321eed879294f3fe49a1f
sadek <mahmoudsadek.1905@gmail.com>
2024-03-31 15:18:03 +0200

    add category contraller

---
+++ Lab1/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using Lab1.DTO;
+using Lab1.Models;
+using Lab1.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id) 
+        {
+            CategoryWithProductNameDTO category =  categoryService.GetById(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public IActionResult Add(CategoryWithoutId category) 
+        {
+            if (ModelState.IsValid)
+            {
+                Category c  = categoryService.Insert(category);
+                return CreatedAtAction("GetById", new { id = c.Id }, category);
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
